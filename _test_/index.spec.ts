6da6d46ba3e08e211c19ce04476585fd8cb537ca
myusuf963 <mohammad963yusuf@gmail.com>
2023-01-15 19:57:01 +0000

    setup testing env

---
+++ _test_/index.spec.ts
@@ -0,0 +1,32 @@
+import {sum} from '../src/index'
+import {expect} from 'chai'
+import assert from 'assert'
+
+describe('sum', () => {
+    it('should return the sum of two numbers', () => {
+        expect('hi').equal("hi")
+        assert.equal('hi',"hi")
+    })
+    it("Should return the sum of two numbers", () => {
+        expect(sum(1, 2)).equal(3);
+        expect(sum(10, 20)).equal(30);
+        expect(sum(-5, 5)).equal(0);
+    });
+
+    it("Should work with decimal numbers", () => {
+        expect(sum(1.5, 2.5)).equal(4);
+        expect(sum(10.25, 20.75)).equal(31);
+    });
+
+    it("Should work with negative numbers", () => {
+        expect(sum(-1, -2)).equal(-3);
+        expect(sum(-10, -20)).equal(-30);
+        expect(sum(-5, 5)).equal(0);
+    });
+
+    it("Should throw an error if input is not a number", () => {
+        // assert.throws(() => sum("1", 2), TypeError, "Input must be a number");
+        // expect(() => sum(1, true)).to.throw(TypeError, "Input must be a number");
+    });
+    
+})
--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-export const add = (a: number, b: number) => {
+ const add = (a: number, b: number) => {
     return a + b
   }
 
-  export const subtract = (a: number, b: number) => {
+const subtract = (a: number, b: number) => {
     return a - b
   }
 
-  export const calculatePI = (radius: number) => {
+const calculatePI = (radius: number) => {
     return radius * radius * Math.PI
   }
